9d24ff3926c57224d003fb95de57b9617534a5fc
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-02-19 00:10:11 +0100

    refactor: Update DynamoDB module to use table_config for improved con… (#7)
    refactor: Update DynamoDB module to use table_config for improved configuration management

--- IDVH/dynamodb/checks.tf
+++ IDVH/dynamodb/checks.tf
@@ -10,7 +10,7 @@ locals {
 
   valid_attribute_types = toset(["S", "N", "B"])
 
-  attribute_names = [for attribute in var.attributes : attribute.name]
+  attribute_names = [for attribute in var.table_config.attributes : attribute.name]
 }
 
 check "dynamodb_yaml_required_keys" {
@@ -67,17 +67,17 @@ check "dynamodb_kms_inputs" {
 check "dynamodb_table_inputs" {
   assert {
     condition = (
-      length(trimspace(var.table_name)) > 0 &&
-      length(trimspace(var.hash_key)) > 0 &&
-      length(var.attributes) > 0 &&
+      length(trimspace(var.table_config.table_name)) > 0 &&
+      length(trimspace(var.table_config.hash_key)) > 0 &&
+      length(var.table_config.attributes) > 0 &&
       length(local.attribute_names) == length(distinct(local.attribute_names)) &&
       alltrue([
-        for attribute in var.attributes :
+        for attribute in var.table_config.attributes :
         length(trimspace(attribute.name)) > 0 && contains(local.valid_attribute_types, attribute.type)
       ]) &&
-      contains(local.attribute_names, var.hash_key)
+      contains(local.attribute_names, var.table_config.hash_key)
     )
 
-    error_message = "Invalid DynamoDB table configuration. Check table_name, hash_key, and attributes."
+    error_message = "Invalid DynamoDB table configuration. Check table_config.table_name, table_config.hash_key, and table_config.attributes."
   }
 }
