66485cf5619bc0a55729ab84f947858ce1e849d5
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2026-02-26 12:17:08 +0100

    fix: Update redeployment trigger to use body variable for API Gateway deployment

--- IDVH/api_gateway/main.tf
+++ IDVH/api_gateway/main.tf
@@ -53,7 +53,7 @@ resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.main.*))
+    redeployment = sha1(var.body)
   }
 
   lifecycle {
